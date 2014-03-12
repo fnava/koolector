8a3f0799d1f0dd732ec7081b11cbe8724be60f86
Fortunato Navarro <fortunato.navarro@gmail.com>
2014-03-12 16:18:09 +0100

    fixing bugs

--- genesis.py
+++ genesis.py
@@ -139,50 +139,9 @@ class genesis(bookLibrary):
 
     def _query_libgen(self): 
         """Returns a dictionary with book data retrieved from mysql libgen db"""
-        filter = """
-SELECT 
-    updated.id, 
-    updated.md5,
-    updated.Title,
-    updated.Author,
-    updated.Year,       /* Filtered, only 2012-2013 */
-    updated.Publisher,
-    updated.Pages,
-    updated.Language,   /* Filtered, only "English" */
-    updated.Extension,  /* Filtered, only "pdf" */
-    updated.Identifier, /* Usually ISBN13.ISBN10 */
-    topics.topic_descr  /* Format topic\\subtopic */
-FROM updated, topics 
-WHERE 
-    updated.Extension='pdf'
-    AND updated.Filename!="" 
-    AND updated.Generic="" 
-    AND updated.Visible=""
-    AND updated.Language="English"
-    AND updated.Year in ("2014","2013")
-    AND (
-        topics.topic_descr like "Computers%"
-        OR topics.topic_descr like "Techn%"
-        OR topics.topic_descr like "Math%"
-        OR topics.topic_descr like "Scien%"
-        )
-    AND updated.topic=topics.topic_id         # id
-        # md5
-        # ISBN
-        # Año
-        # Titulo 
-        # Publisher
-        # num paginas? - filtrado, por generalidad
-        # Language? - filtrado, por generalidad
-        # Format? - filtrado, por generalidad
-    AND topics.lang='en' 
-/* que sentido tiene el group by aquí?
-GROUP BY 
-    updated.Year,topics.topic_descr 
-*/
-ORDER BY 
-    updated.Year DESC
-"""
+        import genesis_sql        
+        filter = genesis_sql.queries["everything_2014_bysize"]
+
         fields = ["id", 
                   "md5",
                   "title",
---
+++ genesis_sql.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 12 15:57:38 2014
+
+@author: fnavarro
+"""
+
+queries = {}
+
+queries["tech_topics_2013_2014"] = """
+SELECT 
+    updated.id, 
+    updated.md5,
+    updated.Title,
+    updated.Author,
+    updated.Year,       /* Filtered, only 2013-2014 */
+    updated.Publisher,
+    updated.Pages,
+    updated.Language,   /* Filtered, only "English" */
+    updated.Extension,  /* Filtered, only "pdf" */
+    updated.Identifier, /* Usually ISBN13.ISBN10 */
+    topics.topic_descr  /* Format topic\\subtopic */
+FROM updated, topics 
+WHERE 
+    updated.Extension='pdf'
+    AND updated.Filename!="" 
+    AND updated.Generic="" 
+    AND updated.Visible=""
+    AND updated.Language="English"
+    AND updated.Year in ("2014","2013")
+    AND (
+        topics.topic_descr like "Computers%"
+        OR topics.topic_descr like "Techn%"
+        OR topics.topic_descr like "Math%"
+        OR topics.topic_descr like "Scien%"
+        )
+    AND updated.topic=topics.topic_id         # id
+        # md5
+        # ISBN
+        # Año
+        # Titulo 
+        # Publisher
+        # num paginas? - filtrado, por generalidad
+        # Language? - filtrado, por generalidad
+        # Format? - filtrado, por generalidad
+    AND topics.lang='en' 
+/* que sentido tiene el group by aquí?
+GROUP BY 
+    updated.Year,topics.topic_descr 
+*/
+ORDER BY 
+    updated.Year DESC
+"""
+
+queries["everything_2014_bysize"] = """
+SELECT 
+    updated.id, 
+    updated.md5,
+    updated.Title,
+    updated.Author,
+    updated.Year,       /* Filtered, only 2013-2014 */
+    updated.Publisher,
+    updated.Pages,
+    updated.Language,   /* Filtered, only "English" */
+    updated.Extension,  /* Filtered, only "pdf" */
+    updated.Identifier, /* Usually ISBN13.ISBN10 */
+    updated.topic  /* Format topic\\subtopic */
+FROM updated
+WHERE updated.Extension =  'pdf'
+    AND updated.Filename !=  ""
+    AND updated.Generic =  ""
+    AND updated.Visible =  ""
+    AND updated.Language =  "English"
+    AND updated.Year IN ("2014")
+ORDER BY  
+    updated.Filesize DESC
+"""
+
